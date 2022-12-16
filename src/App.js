de66eaabd265b95bdef5b74f299fc824348ea44e
Sandeepthapa1 <Sandeepthapa.junior@gmail.com>
2022-12-16 01:13:37 -0600

    custom react hook created

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { Counter, APIFetch, Form } from "./components";
+import { UserDetail } from "./components/APIFetch/userDetails";
+
+function App() {
+  return (
+    <div
+      className="App"
+      style={{ display: "flex", flexDirection: "column", gap: "200px" }}
+    >
+      {/* <Counter /> */}
+      {/* <APIFetch /> */}
+      <Form />
+      {/* <UserDetail /> */}
+    </div>
+  );
+}
+
+export default App;
